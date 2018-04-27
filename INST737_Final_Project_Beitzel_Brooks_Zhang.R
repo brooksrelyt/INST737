adebd88c3efbddb9251d55a8a46afd1c2d564a55
zbeitzel <31458390+zbeitzel@users.noreply.github.com>
2018-04-26 20:41:18 -0400

    Add files via upload

---
+++ INST737_Final_Project_Beitzel_Brooks_Zhang.R
@@ -0,0 +1,16 @@
+#   INST737
+#   Final Project submission from Zac Beitzel, Ty Brooks, and Siwei Zhang
+#   April 26th, 2018
+
+#   Overview of Project:
+#   Our project is creating a predictive analytics model for NCAA Division 1 Men's Basketball, in order to help determine when two teams play against 
+#   each other during the Division 1 post-season single elimination tournaments (March Madness and NIT), which team will win each matchup. 
+
+
+# We stored the 2017-18 season of NCAA Division 1 Men's Basketball basketball team statistics at the below URL on Github, which we read in for use in R
+Season_Stats_201718 <- read.csv(url("https://raw.githubusercontent.com/brooksrelyt/INST737/master/NCAA_Basketball_Season_Stats_2017_18.csv"));
+
+# We stored the 2017-18 post-season single elimination tournament matchups in the following file (along with the actual winner, 
+# as the post-season is now over), so that we can test out predictive model with the actual results of the matchup
+Postseason_Matchups_201718 <- read.csv(url("https://raw.githubusercontent.com/brooksrelyt/INST737/master/NCAA_Basketball_Post_Season_Matchups_2017-18.csv"));
+
